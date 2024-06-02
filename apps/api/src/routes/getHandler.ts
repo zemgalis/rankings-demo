51ec4cfaf693dc5f1ab3736c740acee7034969b1
Maris Zemgalis <maris.zemgalis@gmail.com>
2024-06-02 20:55:17 +0100

    feat(routes): return json response

--- apps/api/src/routes/getHandler.ts
+++ apps/api/src/routes/getHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import assert, { AssertionError } from 'node:assert';
+import fs from 'node:fs';
 import { settings } from '../settings'
 
 export const getHandler = (req: Request, res: Response) => {
@@ -9,8 +10,16 @@ export const getHandler = (req: Request, res: Response) => {
     assert.equal(Object.keys(query).length > 0, true, 'Expected to have query params');
     const { regionId } = query;
     assert.equal(typeof regionId === 'string', true, 'Expected required query param to be set');
-    assert.equal(Number(regionId) === settings.regionId, true, 'Expected required query param to be set')
-    return res.send({ data: { status: 'online' } });
+    assert.equal(Number(regionId) === settings.regionId, true, 'Expected required query param to be set');
+    fs.readFile('./src/data/response.json', 'utf8', (error, data) => {
+      if (error) {
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      const content = JSON.parse(data);
+      res.json(content);
+    });
   } catch (error) {
     if (error instanceof AssertionError) {
       res.status(400);
